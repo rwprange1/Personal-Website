0b7ac7e8200c1e7f185cb2bcda8d774513127d40
rwprange1 <richwp12@gmail.com>
2025-07-29 23:07:00 -0400

    a

--- src/Components/Photos.tsx
+++ src/Components/Photos.tsx
@@ -1,7 +1,7 @@
 import Carousel from "./Carousel";
 
 import grad from '../assets/grad.jpg';
-
+import wf from '../assets/wf.jpg';
 
 
 function Photos(){
@@ -11,7 +11,7 @@ function Photos(){
         <div className="hidden md:flex relative h-108">
             <div className="absolute left-1/2 transform -translate-x-1/2 w-[120%] flex gap-x-12">
             <img src={grad} className="w-1/5  rounded-2xl rotate-355" />
-            <img src="src/assets/wf.jpg" className="w-1/5  rounded-2xl rotate-8" />
+            <img src={wf} className="w-1/5  rounded-2xl rotate-8" />
             <img src="src/assets/bl.jpg" className="w-1/5  rounded-2xl rotate-358" />
             <img src="src/assets/park.jpg" className="w-1/5  rounded-2xl rotate-10" />
             <img src="src/assets/guermo.jpg" className="w-1/5  rounded-2xl rotate-359" />
